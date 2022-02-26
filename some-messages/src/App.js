ee537f9a2db45eaddd4ba413109875a1e4191536
wegfawefgawefg <668es218pur@gmail.com>
2022-02-25 22:14:21 -0800

    i wanna kill myself
    Co-authored-by: Ben Aaron <BenDavidAaron@users.noreply.github.com>
    Co-authored-by: kyle dougan <kdougan@gmail.com>
    Co-authored-by: Sam Claus <samclaus@users.noreply.github.com>

--- some-messages/src/App.js
+++ some-messages/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+import socketIOClient from "socket.io-client";
 
-function App() {
-  let messages = [
+const ENDPOINT = "http://127.0.0.1:5000";
+const messageArray = {
+  data: [
     'good day to you sir',
     'Shut the fuck up John i dont care.',
     'What is your name?',
@@ -11,31 +13,65 @@ function App() {
     'My name is John',
     'How are you?',
     'Hello',
-  ];
+  ]
+};
+
+
+function App() {
+  const [socket, setSocket] = useState(null);
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState(messageArray);
   
-  const [new_message, setNewMessage] = useState('');
-  const [messages_list, setMessagesList] = useState(messages);
+
+  useEffect(() => {
+    const newSocket = socketIOClient(ENDPOINT);
+
+    newSocket.on('connect', () => {
+      console.log('connected to server');
+      newSocket.emit('join', 'an entity has joined');
+    });
+    
+    newSocket.on('join', (data) => {
+      console.log(`join: ${data}`);
+    });
+
+    newSocket.on('message', (msg) => {
+      console.log(`message: ${msg}`);
+      setMessages({data:[msg, ...messages.data]});
+    });
+    
+    setSocket(newSocket);
+    return () => { newSocket.close(); console.log('disconnected from server'); };
+  }, [setSocket]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(new_message);
-    setMessagesList([new_message, ...messages_list]);
-    setNewMessage('');
+    if (!message || message.indexOf(',') !== -1) { return; }
+    console.log(message);
+    socket.emit('message', message);
+    setMessage('');
   }
 
   return (
     <div className="App">
       <body>
         <form onSubmit={handleSubmit}>
-          <input 
+          <input
             type="text" 
-            value={new_message}
-            onChange={event=>setNewMessage(event.target.value)}
-            placeholder="Type a message idiot..." />
+            value={message}
+            onChange={event=>setMessage(event.target.value)}
+            placeholder="type message and hit enter to talk..." 
+            disabled={!socket}
+          />
+          <input type="submit" value="send" disabled={!socket} />
         </form>
-        {messages_list.map(message => (
-          <p>{message}</p>
-        ))}
+        <div className="messages">
+          {messages.data.map((msg, index) => (
+            <pre className="message" key={index}>
+              {msg}
+            </pre>
+          ))}
+        </div>
       </body>
     </div>
   );
